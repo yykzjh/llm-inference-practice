690f1d2ddbdf0118d8d430cbfa0b68386db16462
yiyin.zjh <yiyin.zjh@alibaba-inc.com>
2026-01-07 15:24:28 +0800

    feat: add cuda_profiler util

---
+++ csrc/utils/cuda_profiler.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <iostream>
+
+#include "cuda_profiler.h"
+
+
+namespace tap = torch::autograd::profiler;
+
+size_t CudaProfiler::count = 0;
+
+CudaProfiler::CudaProfiler(const std::string& prefix, std::string user_torch_cuda_profiler_dir): prefix_(prefix) {
+    dest_dir_ = (user_torch_cuda_profiler_dir != "") ? user_torch_cuda_profiler_dir : ".";
+    tap::prepareProfiler(config_, activities_);
+}
+
+CudaProfiler::~CudaProfiler() {
+    if (!stoped_) {
+        stoped_ = true;
+        stop();
+    }
+}
+
+void CudaProfiler::start() {
+    count += 1;
+    stoped_ = false;
+    tap::enableProfiler(config_, activities_);
+}
+
+void CudaProfiler::stop() {
+    std::unique_ptr<tap::ProfilerResult> res       = tap::disableProfiler();
+    std::string                          file_name = dest_dir_ + "/" + prefix_ + std::to_string(count) + ".json";
+    res->save(file_name);
+    stoped_ = true;
+}
---
+++ csrc/utils/cuda_profiler.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <set>
+#include <iostream>
+
+#include <torch/csrc/autograd/profiler_kineto.h>
+
+namespace tpi = torch::profiler::impl;
+
+class CudaProfiler {
+public:
+    CudaProfiler(const std::string& prefix, std::string user_torch_cuda_profiler_dir = "");
+    ~CudaProfiler();
+    void start();
+    void stop();
+
+protected:
+    std::string                 dest_dir_;
+    static size_t               count;
+    std::string                 prefix_;
+    tpi::ProfilerConfig         config_ = tpi::ProfilerConfig(tpi::ProfilerState::KINETO);
+    std::set<tpi::ActivityType> activities_{tpi::ActivityType::CUDA};
+    bool                        stoped_ = true;
+};
